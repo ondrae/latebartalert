71bd5f1d13067ea4a9378b7954c6fd6d01e13e15
Andrew Hyder <hyd415@gmail.com>
2011-05-22 13:56:22 -0700

    testining mysql connection

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-echo "40 + 2 = ".(40+2);
+$db = mysql_connect("db.latebartalert.dotcloud.com:5293",
+                     "root", "B.Hf+]79,sVi[{lfyy3!");
+$result = mysql_query("SELECT 40+2");
+$row = mysql_fetch_row($result);
+$column = $row[0];
+echo "SQL says 40 + 2 = ".$column;
 ?>
