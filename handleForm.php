75c83c77fe0decd644bdeb99cb5777dd804630e3
Andrew Hyder <hyd415@gmail.com>
2011-07-06 00:40:37 -0700

    added handleForm.php

--- handleForm.php
+++ handleForm.php
@@ -1,61 +1,127 @@
 <?php
 include_once 'sqlConnect.php';
 include_once 'cleanStationNames.php';
+//include_once 'model.php';
 
 //Establish variables names from form
-$startStation = $_POST['start_station'];
+$startStation = array($_POST['start_station']);
 $time = $_POST['time'];
-$endStation = $_POST['end_station'];
+$endStation = array($_POST['end_station']);
 $phone = $_POST['phone'];
 
-function get_commute_station_list($startStation, $endStation){
-	//Figure out the list of stations the commuter will travel through.
-	
-	//Get all route numbers
-	$requestRouteNumbers = "http://api.bart.gov/api/route.aspx?cmd=routes&key=".$apiKey;
-	$xml_numbersList = simplexml_load_file($requestRouteNumbers) or die("feed not loading");
-	for($i=0;$i<count($xml_numbersList->routes->route);$i++){
-		$routeNumbers[] = $xml_numbersList->routes->route[$i]->number;
+$apiKey = 'EHDB-ZWQN-EKXT-VV5D';
+
+$startStation = getAbbr($startStation);
+$endStation = getAbbr($endStation);
+$$apiKey = 'EHDB-ZWQN-EKXT-VV5D';
+$startStation = array('24th St. Mission');
+$endStation = array('Powell St.');
+
+//Get list of station names and abbriviations, returns array with names and abbr alternating
+function getStationNames(){
+	$apiKey = "EHDB-ZWQN-EKXT-VV5D";
+	$requestStationList = "http://api.bart.gov/api/stn.aspx?cmd=stns&key=".$apiKey;
+	$xml_stationList = simplexml_load_file($requestStationList) or die("feed not loading");
+		
+	for($i=0;$i<count($xml_stationList->stations->station);$i++){
+		$stationNameAbbrList[] = $xml_stationList->stations->station[$i]->name;
+		$stationNameAbbrList[] = $xml_stationList->stations->station[$i]->abbr;
 	}
-	//echoArray($routeNumbers);
-	
-	//Look through each route station list and keep the stations if Line name is included
-	for($x=0;$x<count($routeNumbers);$x++){
-		$requestStationList = "http://api.bart.gov/api/route.aspx?cmd=routeinfo&route=".$routeNumbers[$x]."&key=".$apiKey;
-		$xml_List = simplexml_load_file($requestStationList) or die("feed not loading");
-		for($i=0;$i<count($xml_List->routes->route->config->station);$i++){
-			$allRouteStations[] = $xml_List->routes->route->config->station[$i];
-			//echoArray($allRouteStations);
+	//print_r($stationNameAbbrList);
+	return $stationNameAbbrList;
+}
+
+
+//Gets abbriviation from station names, return array of abbrs
+function getAbbr($names){
+	//print_r($names);
+	$stationNameAbbrList = getStationNames();
+	for($i=0;$i<count($names);$i++){
+		if($names[$i]=="SFO"){
+			$abbr[] = "SFIA";
+			}
+		if($names[$i]=="East Bay"){
+			$abbr[] = "RICH";
+			$abbr[] = "PITT";
+			$abbr[] = "DUBL";
+			$abbr[] = "FRMT";
+			}
+		if($names[$i]=="San Francisco"){
+			$abbr[] = "SFIA";
+			$abbr[] = "MLBR";
+			$abbr[] = "DALY";
+			}
+		$nameKey = array_search($names[$i], $stationNameAbbrList);
+		if($nameKey!=""){
+			$abbr[] = $stationNameAbbrList[$nameKey+1];
+			}
 		}
-		for($a=0;$a<count($routeFirstAbbr);$a++){
-			if(in_array($routeFirstAbbr[$a], $allRouteStations)){
-				//echo 'Boogaloo!';
-				//echo($allRouteStations);
-				for($z=0;$z<count($allRouteStations);$z++){
-					$delayedStations[] = $allRouteStations[$z];
+	$abbr = cleanArray($abbr);
+	//print_r($abbr);
+	return $abbr;
+	}
+	
+//Make an array contain only strings
+function cleanArray($array){
+	//print_r($array);
+	for($i=0;$i<count($array);$i++){
+		$array[$i] = strval($array[$i]);
+	}
+	//print_r($array);
+	return $array;
+}	
+
+$startStation = getAbbr($startStation);
+$endStation = getAbbr($endStation);
+//print_r($startStation);
+//print_r($endStation);
+	
+//Get all route numbers
+$requestRouteNumbers = "http://api.bart.gov/api/route.aspx?cmd=routes&key=".$apiKey;
+$xml_numbersList = simplexml_load_file($requestRouteNumbers) or die("feed not loading");
+foreach($xml_numbersList->routes->route as $num){
+	$routeNumbers[] = $num;
+}
+//Look through each route station list and keep the stations if $start and $end stations are included
+foreach ($routeNumbers as $num){
+	//Start with empty array each loop
+	$fullStationList = array();
+	$requestStationList = "http://api.bart.gov/api/route.aspx?cmd=routeinfo&route=".$num."&key=".$apiKey;
+	//print $requestStationList;
+	$xml_List = simplexml_load_file($requestStationList) or die("feed not loading");
+	foreach($xml_List->routes->route->config->station as $station){
+		$fullStationList[] = $station;	
+	}
+	//print 'Station list for route number '.$num.'<br/>';
+	//print '<br/> The Starting station: '.$startStation[0].'<br/>';	
+	$startKey = array_search($startStation[0], $fullStationList);
+	//print '<br/>FOund start key: '.$startKey.'<br/>';
+	if(is_int($startKey)){
+		$endKey = array_search($endStation[0], $fullStationList);
+		//print '<br/>Found end key: '.$endKey.'<br/>';
+		//Figure out order of keys
+		if(is_int($endKey)){
+			if($endKey>$startKey){
+				for($i=0;$i<=($endKey-$startKey);$i++){
+					$commuterStations[] = $fullStationList[$i+$startKey];
+					}
+				//print_r($commuterStations);
+				break;
+				}
+			elseif($startKey>$endKey){
+				for($i=0;$i<=($startKey-$endKey);$i++){
+					$commuterStations[] = $fullStationList[$i+$endKey];
+					}
+				//print_r($commuterStations);
+				break;
 				}
 			}
 		}
-		unset($allRouteStations);	
 	}
-	//echoArray($delayedStations);
-	$delayedStations = array_unique($delayedStations);
-	return $delayedStations;
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	return $commute_station_list	
-}
 
+	
 //Establish query to insert data to MySQL table
-$insert = "INSERT INTO $table VALUES ('','$start_station','$time','$end_station','$phone', '$commute_station_list')";
+$insert = "INSERT INTO $table VALUES ('','$start_station','$time','$end_station','$phone', '$commuterStations')";
 
 //Insert the data
 mysql_query($insert);
