0041ea5696c3653dfaa8b25f4c5d79d6dab79dc7
Andrew Hyder <hyd415@gmail.com>
2011-07-16 13:28:42 -0700

    testing

--- sqlConnect.php
+++ sqlConnect.php
@@ -2,12 +2,13 @@
 
 $apiKey = 'EHDB-ZWQN-EKXT-VV5D';
 
-$host = 'localhost';
+$host = 'db.latebart.dotcloud.com:10926';
 $password = '7dsfjkh78';
 $username = 'latebarter';
 $database = 'latebart';
 mysql_connect($host,$username,$password) or die('woops');
 mysql_select_db($database);
+echo 'Hi!';
 
 
 ?>
