b8d6ef071dbf5ed273d273549793f153496badf6
Andrew Hyder <hyd415@gmail.com>
2011-07-16 13:23:07 -0700

    testing

--- sqlConnect.php
+++ sqlConnect.php
@@ -4,7 +4,7 @@ $apiKey = 'EHDB-ZWQN-EKXT-VV5D';
 
 //Opens the MySQL Database, remember to close it!
 function openDatabase(){
-	$host = 'mysql@2810db75.dotcloud.com:10926';
+	$host = 'localhost';
 	$password = '7dsfjkh78';
 	$username = 'latebarter';
 	$database = 'latebart';
