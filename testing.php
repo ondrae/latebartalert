625385a0733acbc44790ed56cbb5406142b16e5c
Andrew Hyder <hyd415@gmail.com>
2011-07-16 13:21:56 -0700

    testing

--- testing.php
+++ testing.php
@@ -1,1 +1,14 @@
-<?php phpinfo(); ?>
+<?
+include sqlConnect.php
+
+openDatabase();
+$result = mysql_query('SELECT * from formAnswers');
+if (!$result) {
+    die('Invalid query: ' . mysql_error());
+}
+
+echo $results;
+mysql_close();
+ 
+
+?>
