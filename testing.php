ff43d37e297a840e85ec2e6b0612dd0ad0b90921
Andrew Hyder <hyd415@gmail.com>
2011-07-16 15:09:49 -0700

    testing

--- testing.php
+++ testing.php
@@ -2,13 +2,13 @@
 
 $envjson = json_decode(file_get_contents("/home/dotcloud/environment.json"),true);
 
-# Create MySQL Connection
-$mysqli = new mysqli($envjson['DOTCLOUD_DB_MYSQL_HOST'],
-                     $envjson['DOTCLOUD_DB_MYSQL_LOGIN'],         # username
-                     $envjson['DOTCLOUD_DB_MYSQL_PASSWORD'],   # password
-                     'latebart',       # db name
-                     $envjson['DOTCLOUD_DB_MYSQL_PORT']);
-
-print_r($mysqli->query('SELECT * from formAnswers;'));
-
+#Create MySQL Connection
+$db = mysql_connect($envjson['DOTCLOUD_DB_MYSQL_HOST'],
+					$envjson['DOTCLOUD_DB_MYSQL_LOGIN'],
+					$envjson['DOTCLOUD_DB_MYSQL_PASSWORD']);
+$result = mysql_query("SELECT 40+2");
+$row = mysql_fetch_row($result);
+$column = $row[0];
+echo "SQL says 40 + 2 = ".$column;
 ?>
+
