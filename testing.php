992616a3d7eb70023a15d68df726ab5e821565b1
Andrew Hyder <hyd415@gmail.com>
2011-07-16 15:05:16 -0700

    testing

--- testing.php
+++ testing.php
@@ -5,7 +5,7 @@ $envjson = json_decode(file_get_contents("/home/dotcloud/environment.json"),true
 # Create MySQL Connection
 $mysqli = new mysqli($envjson['DOTCLOUD_DB_MYSQL_HOST'],
                      $envjson['DOTCLOUD_DB_MYSQL_LOGIN'],         # username
-                     $envjson['DOTCLOUD_DB_MYSQL_HOST'],   # password
+                     $envjson['DOTCLOUD_DB_MYSQL_PASSWORD'],   # password
                      'latebart',       # db name
                      $envjson['DOTCLOUD_DB_MYSQL_PORT']);
 
