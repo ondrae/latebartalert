6da5b826d722a5847cef99c487fa9f11dee396f1
Andrew Hyder <hyd415@gmail.com>
2011-07-16 14:49:24 -0700

    testing

--- testing.php
+++ testing.php
@@ -3,12 +3,10 @@
 # Import environment settings from DotCloud
 $envjson = json_decode(file_get_contents("/home/dotcloud/environment.json"),true);
 
-print_r($envjson);
-
 # Create MySQL Connection
-mysql_connect($envjson['DOTCLOUD_DB_MYSQL_HOST'],
-                     'latebarter',         # username
-                     '7dsfjkh78',   # password
+mysql_connect($envjson['DOTCLOUD_DB_MYSQL_HOST'], #host
+                     $envjson['DOTCLOUD_DB_MYSQL_LOGIN'],         # username
+                     $envjson['DOTCLOUD_DB_MYSQL_PASSWORD'],   # password
                      $envjson['DOTCLOUD_DB_MYSQL_PORT']) or die('Can\'t connect');
                      
 mysql_select_db($database);
