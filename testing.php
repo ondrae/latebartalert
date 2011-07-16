7bcf00f233f00c6e2312f25f5e4b6028e3f91133
Andrew Hyder <hyd415@gmail.com>
2011-07-16 13:30:26 -0700

    testing

--- testing.php
+++ testing.php
@@ -1,7 +1,13 @@
 <?
-include sqlConnect.php
+$host = 'db.latebart.dotcloud.com:10926';
+$password = '7dsfjkh78';
+$username = 'latebarter';
+$database = 'latebart';
+mysql_connect($host,$username,$password) or die('woops');
+mysql_select_db($database);
+echo 'Hi!';
 
-$result = mysql_query('SELECT * from formAnswers');
+$result = mysql_query('SELECT * from formAnswers;');
 if (!$result) {
     die('Invalid query: ' . mysql_error());
 }
