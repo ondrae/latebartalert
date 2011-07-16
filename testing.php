f590cc2f75dfbdb8495306f9194616e5682cbc9b
Andrew Hyder <hyd415@gmail.com>
2011-07-16 13:35:24 -0700

    testing

--- testing.php
+++ testing.php
@@ -1,5 +1,5 @@
 <?
-$host = 'mysql://root:hfzedh6K4CspuJzVKiaT@2810db75.dotcloud.com:10927';
+$host = '2810db75.dotcloud.com:10927';
 $password = '7dsfjkh78';
 $username = 'latebarter';
 $database = 'latebart';
