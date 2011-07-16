4f925fc4f829418cbc9189c86d636dc003b095f3
Andrew Hyder <hyd415@gmail.com>
2011-07-16 13:46:50 -0700

    testing

--- testing.php
+++ testing.php
@@ -1,15 +1,19 @@
 <?php
-$host = 'db.dotcloud.com:10927';
-$password = '7dsfjkh78';
-$username = 'latebarter';
+$envfilepath = $_ENV['HOME'] + '/environment.json'
+$environment = json_decode(file_get_contents($envfilepath),true);
+$db_host = $environment['DOTCLOUD_DATA_MYSQL_HOST'];
+$db_port = $environment['DOTCLOUD_DATA_MYSQL_PORT'];
+$db_username = $environment['DOTCLOUD_DATA_MYSQL_LOGIN'];
+$db_password = $environment['DOTCLOUD_DATA_MYSQL_PASSWORD'];
 $database = 'latebart';
-mysql_connect($host,$username,$password) or die('woops');
+
+mysql_connect($db_host,$db_username,$db_password) or die('woops');
 mysql_select_db($database);
 
 $result = mysql_query('SELECT startStation from formAnswers limit 1;');
 
-
-echo $results;
+print $result;
+echo $result;
 mysql_close();
  
 
