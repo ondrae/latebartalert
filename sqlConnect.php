44723192a5f373e393989b17260ac74106f3d4ea
Andrew Hyder <hyd415@gmail.com>
2011-07-16 13:17:43 -0700

    blah

--- sqlConnect.php
+++ sqlConnect.php
@@ -4,10 +4,7 @@ $apiKey = 'EHDB-ZWQN-EKXT-VV5D';
 
 //Opens the MySQL Database, remember to close it!
 function openDatabase(){
-	$envfilepath = $_ENV['HOME'] + '/environment.json'
-	$environment = json_decode(file_get_contents($envfilepath),true);
 	$host = 'mysql@2810db75.dotcloud.com:10926';
-	$db_port = $environment['DOTCLOUD_DATA_MYSQL_PORT'];
 	$password = '7dsfjkh78';
 	$username = 'latebarter';
 	$database = 'latebart';
