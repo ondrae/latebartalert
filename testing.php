67e5ee9a1f6b85f3b36af3cb0a8665eeb3fad655
Andrew Hyder <hyd415@gmail.com>
2011-07-16 15:10:59 -0700

    testing

--- testing.php
+++ testing.php
@@ -3,7 +3,8 @@
 $envjson = json_decode(file_get_contents("/home/dotcloud/environment.json"),true);
 
 #Create MySQL Connection
-$db = mysql_connect($envjson['DOTCLOUD_DB_MYSQL_HOST'],
+$db = mysql_connect($envjson['DOTCLOUD_DB_MYSQL_HOST']
+					.':'.$envjson['DOTCLOUD_DB_MYSQL_PORT'],
 					$envjson['DOTCLOUD_DB_MYSQL_LOGIN'],
 					$envjson['DOTCLOUD_DB_MYSQL_PASSWORD']);
 $result = mysql_query("SELECT 40+2");
