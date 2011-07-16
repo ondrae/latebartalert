162803a99d9e13fcdeb6d20711034cf0a6170f0b
Andrew Hyder <hyd415@gmail.com>
2011-07-16 14:59:16 -0700

    testing

--- testing.php
+++ testing.php
@@ -1,23 +1,14 @@
 <?php
 
-# Import environment settings from DotCloud
 $envjson = json_decode(file_get_contents("/home/dotcloud/environment.json"),true);
 
-#print_r($envjson);
-
 # Create MySQL Connection
-mysql_connect($envjson['DOTCLOUD_DB_MYSQL_HOST'].':'.$envjson['DOTCLOUD_DB_MYSQL_PORT'], $envjson['DOTCLOUD_DB_MYSQL_LOGIN'], $envjson['DOTCLOUD_DB_MYSQL_PASSWORD']) or die('Can\'t connect');
-                     
-mysql_select_db($database);
+$mysqli = new mysqli($envjson['DOTCLOUD_DB_MYSQL_HOST'],
+                     $envjson['DOTCLOUD_DB_MYSQL_LOGIN'],         # username
+                     $envjson['DOTCLOUD_DB_MYSQL_HOST'],   # password
+                     'latebart',       # db name
+                     $envjson['DOTCLOUD_DB_MYSQL_PORT']);
 
-$result = mysql_query('SELECT startStation from formAnswers;');
-
-print $result;
-echo $result;
-mysql_close();
- 
-
-
-
+echo $mysqli->query('SELECT * from formAnswers;'));
 
 ?>
