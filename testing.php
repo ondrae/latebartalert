0ac063b9dda11e5a124827ae28b457acda543b21
Andrew Hyder <hyd415@gmail.com>
2011-07-16 13:52:35 -0700

    testing

--- testing.php
+++ testing.php
@@ -1,14 +1,14 @@
 <?php
 $envfilepath = $_ENV['HOME'] + '/environment.json';
 $environment = json_decode(file_get_contents($envfilepath),true);
-$db_host = $environment['DOTCLOUD_DATA_MYSQL_HOST'];
-$db_port = $environment['DOTCLOUD_DATA_MYSQL_PORT'];
-$db_username = $environment['DOTCLOUD_DATA_MYSQL_LOGIN'];
-$db_password = $environment['DOTCLOUD_DATA_MYSQL_PASSWORD'];
-$database = 'latebart';
+$db_host = $environment['DOTCLOUD_DB_MYSQL_HOST'];
+$db_port = $environment['DOTCLOUD_DB_MYSQL_PORT'];
+$db_username = $environment['DOTCLOUD_DB_MYSQL_LOGIN'];
+$db_password = $environment['DOTCLOUD_DB_MYSQL_PASSWORD'];
+$DBbase = 'latebart';
 
 mysql_connect($db_host,$db_username,$db_password) or die('woops');
-mysql_select_db($database);
+mysql_select_db($DBbase);
 
 $result = mysql_query('SELECT startStation from formAnswers;');
 
