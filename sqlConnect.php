b0c0de6555156d29a3ef6216cebe88cbc6dd3645
Andrew Hyder <hyd415@gmail.com>
2011-09-07 11:45:17 -0700

    Starting alert checker

--- sqlConnect.php
+++ sqlConnect.php
@@ -1,14 +1,16 @@
 <?php
-#BART API KEY
+//BART API KEY
 $apiKey = 'EHDB-ZWQN-EKXT-VV5D';
 
 function openDatabase(){
 	$envjson = json_decode(file_get_contents("/home/dotcloud/environment.json"),true);
-	#Create MySQL Connection
+	//Create MySQL Connection
 	mysql_connect($envjson['DOTCLOUD_DB_MYSQL_HOST']
 				  	.':'.$envjson['DOTCLOUD_DB_MYSQL_PORT'],
 					$envjson['DOTCLOUD_DB_MYSQL_LOGIN'],
 					$envjson['DOTCLOUD_DB_MYSQL_PASSWORD']) or die('Can\'t Connect');
 	mysql_select_db('latebart');
 }
+
+//Remember to close your mysql connection
 ?>
