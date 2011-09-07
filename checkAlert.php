d2c03fbbb10a1d7c0fabeafe3691cfbf7cb387e5
Andrew Hyder <hyd415@gmail.com>
2011-09-07 12:10:22 -0700

    Starting on check Alert

--- checkAlert.php
+++ checkAlert.php
@@ -10,24 +10,26 @@ function checkAlert(){
 	$currentStation = $xml_currentAdvisory->bsa->station;
 	$currentAdvisory = $xml_currentAdvisory->bsa->description;
 	
+	echo $currentAdvisory;
+	
 	//If no advisories then say so and be done
 	if ($currentAdvisory == 'No delays reported'){
-		echo $currentAdvisory;
+		//echo $currentAdvisory;
 	} else {
 		//Else check in mysql for last advisory to see if its changed
 		$table = "advisories";
 		openDatabase();
 		$query="SELECT * FROM $table ORDER BY id DESC LIMIT 1";
 		$advisory=mysql_query($query);
-		echo $advisory;
+		//echo $advisory;
 		//Latest advisory in table		
 		$num=mysql_numrows($advisoriesy) - 1;
 		$lastAdvisory=mysql_result($advisory,$num,"advisory");
 		if($currentAdvisory == $lastAdvisory){
-			echo "The current advisory $currentAdvisory is still in effect <br />";
+			//echo "The current advisory $currentAdvisory is still in effect";
 			mysql_close();
 		} else {
-			echo "There is a brand new advisory of $currentAdvisory <br />";
+			//echo "There is a brand new advisory of $currentAdvisory <br />";
 			$insert = "INSERT INTO $table VALUES ('','$currentStation','$currentAdvisory')";
 			mysql_query($insert);
 			mysql_close();
