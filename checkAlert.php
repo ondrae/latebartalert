04d4243f123bf9fb51a58f152dbe01e478abfed7
Andrew Hyder <hyd415@gmail.com>
2011-09-07 12:22:46 -0700

    Starting on check Alert

--- checkAlert.php
+++ checkAlert.php
@@ -5,36 +5,31 @@ function checkAlert(){
 	
 	$requestCurrentAdvisory = "http://api.bart.gov/api/bsa.aspx?cmd=bsa&key=$apiKey";
 	$xml_currentAdvisory = simplexml_load_file($requestCurrentAdvisory) or die("feed not loading");
-	echo $xml_currentAdvisory;
 	
 	//Set Station variable - Currently always system wide BART
 	$currentStation = $xml_currentAdvisory->bsa->station;
 	$currentAdvisory = $xml_currentAdvisory->bsa->description;
 	
-	echo $currentAdvisory;
-	
 	//If no advisories then say so and be done
 	if ($currentAdvisory == 'No delays reported'){
-		//echo $currentAdvisory;
+		echo $currentAdvisory;
 	} else {
 		//Else check in mysql for last advisory to see if its changed
 		$table = "advisories";
 		openDatabase();
 		$query="SELECT * FROM $table ORDER BY id DESC LIMIT 1";
-		$advisory=mysql_query($query);
-		//echo $advisory;
-		//Latest advisory in table		
-		$num=mysql_numrows($advisoriesy) - 1;
-		$lastAdvisory=mysql_result($advisory,$num,"advisory");
+		$lastAdvisory=mysql_query($query);
+		echo $lastAdvisory;
+			
 		if($currentAdvisory == $lastAdvisory){
-			//echo "The current advisory $currentAdvisory is still in effect";
+			echo "The current advisory $currentAdvisory is still in effect";
 			mysql_close();
 		} else {
-			//echo "There is a brand new advisory of $currentAdvisory <br />";
+			echo "There is a brand new advisory of $currentAdvisory";
 			$insert = "INSERT INTO $table VALUES ('','$currentStation','$currentAdvisory')";
 			mysql_query($insert);
 			mysql_close();
-			}
+		}
 	}
 } 
 
