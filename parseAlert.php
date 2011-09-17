07cb7dc6ca80857d4ce5b1be74af7541cde5f9c5
Andrew Hyder <hyd415@gmail.com>
2011-09-17 13:57:07 -0700

    testing alert parsing

--- parseAlert.php
+++ parseAlert.php
@@ -8,27 +8,30 @@ echo "<center>";
 
 $lastAlert = getLastAlert($whichAlert);
 print_r($lastAlert);
-echoArray($lastAlert);
+//echoArray($lastAlert);
 
 $lastAlert = cleanAlert($lastAlert);
-print_r($lastAlert);
-echoArray($lastAlert);
+//print_r($lastAlert);
+//echoArray($lastAlert);
 
 echo "<br />Delayed Direction(s)<br />";
 $direction = getDirection($lastAlert);
-echoArray($direction);
+//echoArray($direction);
 print_r($direction);
 
-echo "<br />Delayed Direction(s) Abbriviation<br />";
-$directionAbbr = getAbbr($direction);
-echoArray($directionAbbr);
-print_r($directionAbbr);
-
-echo "<br />Delayed Station(s) Abbriviation<br />";
-$delayedStations = getDelayedStations($lastAlert);
-print_r($delayedStations);
-echoArray($delayedStations);
-
+if($direction!='') {
+	echo "<br />Delayed Direction(s) Abbriviation<br />";
+	$directionAbbr = getAbbr($direction);
+	//echoArray($directionAbbr);
+	print_r($directionAbbr);
+	
+	echo "<br />Delayed Station(s) Abbriviation<br />";
+	$delayedStations = getDelayedStations($lastAlert);
+	print_r($delayedStations);
+	//echoArray($delayedStations);
+}else{
+echo 'No Delays';
+}
 echo "</center>";
 
 /*--------------------------------------------------------------------*/
