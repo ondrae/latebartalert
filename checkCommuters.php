7af8aa25bd8ae9cc775ff4bc751bfaaaa6522ffb
Andrew Hyder <hyd415@gmail.com>
2011-09-17 13:06:22 -0700

    hmmm

--- checkCommuters.php
+++ checkCommuters.php
@@ -1,14 +1,14 @@
 <?php
 function checkCommuters(){
 	include 'sqlConnect.php';
-	echo $date = date("H:i:s");
+	echo date("g:i a", strtotime("now"))
 	echo "\n";
 	$table = "formAnswers";
 	openDatabase();
 	$query="SELECT * FROM $table";
 	$result=mysql_query($query);
 	while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
-   		printf("ID: %s  Start Station: %s Time: %s End Station: %s Phone: %s Commute Stations: %s", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5]);  
+   		printf("ID: %s  Start Station: %s Time: %s End Station: %s Phone: %s Commute Stations: %s", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5]); 
 	}
 	//echo $lastAdvisory;
 	mysql_close();
