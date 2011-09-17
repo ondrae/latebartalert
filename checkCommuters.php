0fe028b573bc3abfbcfed06e03498423cfb86f66
Andrew Hyder <hyd415@gmail.com>
2011-09-17 12:46:44 -0700

    testing commuter checking

--- checkCommuters.php
+++ checkCommuters.php
@@ -1,19 +1,18 @@
 <?php
 function checkCommuters(){
 	include 'sqlConnect.php';
-		echo $date = date("Y-m-d H:i:s");
-		echo "\n";
-		$table = "formAnswers";
-		openDatabase();
-		$query="SELECT * FROM $table";
-		$result=mysql_query($query);
-		//$lastAdvisory = mysql_result($result, 0);
-		//echo $lastAdvisory;
-		mysql_close();
-		//include sendAlerts();
-		}
-	}
-} 
+	echo $date = date("H:i:s");
+	echo "\n";
+	$table = "formAnswers";
+	openDatabase();
+	$query="SELECT * FROM $table";
+	$result=mysql_query($query);
+	echo $result;
+	//$lastAdvisory = mysql_result($result, 0);
+	//echo $lastAdvisory;
+	mysql_close();
+	//include sendAlerts();
+}
 
 checkCommuters();
 ?>
