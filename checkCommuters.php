8e6fdf9b1e98b3ce0d0d2fcc6d5c9011b0ae3637
Andrew Hyder <hyd415@gmail.com>
2011-09-17 13:03:07 -0700

    hmmm

--- checkCommuters.php
+++ checkCommuters.php
@@ -7,8 +7,9 @@ function checkCommuters(){
 	openDatabase();
 	$query="SELECT * FROM $table";
 	$result=mysql_query($query);
-	echo $result;
-	//$lastAdvisory = mysql_result($result, 0);
+	while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
+   		printf("ID: %s  Start Station: %s Time: %s End Station: %s Phone: %s Commute Stations: %s", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5]);  
+	}
 	//echo $lastAdvisory;
 	mysql_close();
 	//include sendAlerts();
