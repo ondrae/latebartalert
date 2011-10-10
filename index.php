a4bed2c5b0b795521dc98a1a9c60f0c254f5fc7f
Andrew Hyder <hyd415@gmail.com>
2011-10-09 21:51:24 -0700

    messing with the design

--- index.php
+++ index.php
@@ -30,165 +30,173 @@ function echoArray($array){
 
 	<head>
 		<meta charset="utf-8" />
-		<title>Late BART Alert</title>
+		<title>Late BART</title>
 		<!--[if IE]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
 		<!--<LINK REL=StyleSheet HREF="html5Reset.css" TYPE="text/css" MEDIA=screen>-->
-		<LINK REL=StyleSheet HREF="lateBartStyle.css" TYPE="text/css" MEDIA=screen>
+		<link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.3.0/bootstrap.min.css">
+		<!--<LINK REL=StyleSheet HREF="lateBartStyle.css" TYPE="text/css" MEDIA=screen>-->
 		
 	
 	</head>
 	
 <body>
-	
-	
 	<header>
-	<div id="hat">
-	</div><!--/hat-->
-	
-	<div id="head">
-	
-		<div id="title">
-			<a href="http://www.latebartalert.com"><img src="lateBartAlertLogoWhiteBG.original.png" alt="" /></a>
-		</div><!--/title-->
-	
-		<div id="tagline">
-			Late BART Alert will send you a text message if your train is going to be late. Sign up now to join our beta.
-		</div><!--/tagline-->	
-	
-	</div><!--/head-->		
+		<div class="topbar">
+			<div class="fill">
+				<div class="container">
+					<a class="brand" href="#">Late BART</a>
+					<ul class="nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#about">About</a></li>
+					<li><a href="#contact">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>		
 	</header>
 	
-	<div id="sectionBackground">
-	<section>	
-		<form method="post" action="handleForm.php">
-			<fieldset>
-			<h2>Which Train Do you Catch?</h2>
-			<ol>	
-				<li>
-				<label for="startStation">Station you start at:</label> 
-				<select name="startStation" required>
-					<?php
-					listStations($xml_stationList);
-					?>
-				</select>
-				</li>
-				
-				<li>	
-				<label>What time do you catch your train?</label>
-				<select name="time" placeholder="Best guess is cool" type="text" />
-					<option value="4:00 am">4:00 am</option>
-					<option value="4:15 am">4:15 am</option>
-					<option value="4:30 am">4:30 am</option>
-					<option value="4:45 am">4:45 am</option>
-					<option value="5:00 am">5:00 am</option>
-					<option value="5:15 am">5:15 am</option>
-					<option value="5:30 am">5:30 am</option>
-					<option value="5:45 am">5:45 am</option>
-					<option value="6:00 am">6:00 am</option>
-					<option value="6:15 am">6:15 am</option>
-					<option value="6:30 am">6:30 am</option>
-					<option value="6:45 am">6:45 am</option>
-					<option value="7:00 am">7:00 am</option>
-					<option value="7:15 am">7:15 am</option>
-					<option value="7:30 am">7:30 am</option>
-					<option value="7:45 am">7:45 am</option>
-					<option value="8:00 am">8:00 am</option>
-					<option value="8:15 am">8:15 am</option>
-					<option value="8:30 am">8:30 am</option>
-					<option value="8:45 am">8:45 am</option>
-					<option value="9:00 am">9:00 am</option>
-					<option value="9:15 am">9:15 am</option>
-					<option value="9:30 am">9:30 am</option>
-					<option value="9:45 am">9:45 am</option>
-					<option value="10:00 am">10:00 am</option>
-					<option value="10:15 am">10:15 am</option>
-					<option value="10:30 am">10:30 am</option>
-					<option value="10:45 am">10:45 am</option>
-					<option value="11:00 am">11:00 am</option>
-					<option value="11:15 am">11:15 am</option>
-					<option value="11:30 am">11:30 am</option>
-					<option value="11:45 am">11:45 am</option>
-					<option value="12:00 pm">12:00 pm</option>
-					<option value="12:15 pm">12:15 pm</option>
-					<option value="12:30 pm">12:30 pm</option>
-					<option value="12:45 pm">12:45 pm</option>
-					<option value="1:00 pm">1:00 pm</option>
-					<option value="1:15 pm">1:15 pm</option>
-					<option value="1:30 pm">1:30 pm</option>
-					<option value="1:45 pm">1:45 pm</option>
-					<option value="2:00 pm">2:00 pm</option>
-					<option value="2:15 pm">2:15 pm</option>
-					<option value="2:30 pm">2:30 pm</option>
-					<option value="2:45 pm">2:45 pm</option>
-					<option value="3:00 pm">3:00 pm</option>
-					<option value="3:45 pm">3:45 pm</option>
-					<option value="4:00 pm">4:00 pm</option>
-					<option value="4:15 pm">4:15 pm</option>
-					<option value="4:30 pm">4:30 pm</option>
-					<option value="4:45 pm">4:45 pm</option>
-					<option value="5:00 pm">5:00 pm</option>
-					<option value="5:15 pm">5:15 pm</option>
-					<option value="5:30 pm">5:30 pm</option>
-					<option value="5:45 pm">5:45 pm</option>
-					<option value="6:00 pm">6:00 pm</option>
-					<option value="6:15 pm">6:15 pm</option>
-					<option value="6:30 pm">6:30 pm</option>
-					<option value="6:45 pm">6:45 pm</option>
-					<option value="7:00 pm">7:00 pm</option>
-					<option value="7:15 pm">7:15 pm</option>
-					<option value="7:30 pm">7:30 pm</option>
-					<option value="7:45 pm">7:45 pm</option>
-					<option value="8:00 pm">8:00 pm</option>
-					<option value="8:15 pm">8:15 pm</option>
-					<option value="8:30 pm">8:30 pm</option>
-					<option value="8:45 pm">8:45 pm</option>
-					<option value="9:00 pm">9:00 pm</option>
-					<option value="9:15 pm">9:15 pm</option>
-					<option value="9:30 pm">9:30 pm</option>
-					<option value="9:45 pm">9:45 pm</option>
-					<option value="10:00 pm">10:00 pm</option>
-					<option value="10:15 pm">10:15 pm</option>
-					<option value="10:30 pm">10:30 pm</option>
-					<option value="10:45 pm">10:45 pm</option>
-					<option value="11:00 pm">11:00 pm</option>
-					<option value="11:15 pm">11:15 pm</option>
-					<option value="11:30 pm">11:30 pm</option>
-					<option value="11:45 pm">11:45 pm</option>
-					<option value="12:00 am">12:00 am</option>
-					<option value="12:15 am">12:15 am</option>
-					<option value="12:30 am">12:30 am</option>
-				</select>
-				</li>
-				
-				<li>	
-				<label for="endStation">Station you end at:</label>
-				<select name="endStation" required>
-					<?php
-					listStations($xml_stationList);
-					?>
-				</select>
-				</li>
-			
-				<li>
-				<label>Phone number:</label>
-				<input type="text" name="phone" placeholder="415-NOT-LATE" /> <br />					</li>
-				
-				<li class="submit">
-				<input type="submit" value="Sign Up" />
-				</li>
-			
-			</ol>
-			</fieldset>
-		</form>
+	<section>
+		<div class="container">
+		  <div class="hero-unit">
+			<h1>Late BART</h1>
+			<p>Late BART will send you a text message if your train is going to be late.</p>
+		  </div>
+		
+			<div class="row">
+				<div class="span10 offset3">
+					<form method="post" action="handleForm.php">
+						<fieldset>
+							<h2>Which Train Do you Catch?</h2>
+								<ol>	
+								<li>
+								<label for="startStation">Station you start at:</label> 
+								<select name="startStation" required>
+									<?php
+									listStations($xml_stationList);
+									?>
+								</select>
+								</li>
+								
+								<li>	
+								<label>What time do you catch your train?</label>
+								<select name="time" placeholder="Best guess is cool" type="text" />
+									<option value="4:00 am">4:00 am</option>
+									<option value="4:15 am">4:15 am</option>
+									<option value="4:30 am">4:30 am</option>
+									<option value="4:45 am">4:45 am</option>
+									<option value="5:00 am">5:00 am</option>
+									<option value="5:15 am">5:15 am</option>
+									<option value="5:30 am">5:30 am</option>
+									<option value="5:45 am">5:45 am</option>
+									<option value="6:00 am">6:00 am</option>
+									<option value="6:15 am">6:15 am</option>
+									<option value="6:30 am">6:30 am</option>
+									<option value="6:45 am">6:45 am</option>
+									<option value="7:00 am">7:00 am</option>
+									<option value="7:15 am">7:15 am</option>
+									<option value="7:30 am">7:30 am</option>
+									<option value="7:45 am">7:45 am</option>
+									<option value="8:00 am">8:00 am</option>
+									<option value="8:15 am">8:15 am</option>
+									<option value="8:30 am">8:30 am</option>
+									<option value="8:45 am">8:45 am</option>
+									<option value="9:00 am">9:00 am</option>
+									<option value="9:15 am">9:15 am</option>
+									<option value="9:30 am">9:30 am</option>
+									<option value="9:45 am">9:45 am</option>
+									<option value="10:00 am">10:00 am</option>
+									<option value="10:15 am">10:15 am</option>
+									<option value="10:30 am">10:30 am</option>
+									<option value="10:45 am">10:45 am</option>
+									<option value="11:00 am">11:00 am</option>
+									<option value="11:15 am">11:15 am</option>
+									<option value="11:30 am">11:30 am</option>
+									<option value="11:45 am">11:45 am</option>
+									<option value="12:00 pm">12:00 pm</option>
+									<option value="12:15 pm">12:15 pm</option>
+									<option value="12:30 pm">12:30 pm</option>
+									<option value="12:45 pm">12:45 pm</option>
+									<option value="1:00 pm">1:00 pm</option>
+									<option value="1:15 pm">1:15 pm</option>
+									<option value="1:30 pm">1:30 pm</option>
+									<option value="1:45 pm">1:45 pm</option>
+									<option value="2:00 pm">2:00 pm</option>
+									<option value="2:15 pm">2:15 pm</option>
+									<option value="2:30 pm">2:30 pm</option>
+									<option value="2:45 pm">2:45 pm</option>
+									<option value="3:00 pm">3:00 pm</option>
+									<option value="3:45 pm">3:45 pm</option>
+									<option value="4:00 pm">4:00 pm</option>
+									<option value="4:15 pm">4:15 pm</option>
+									<option value="4:30 pm">4:30 pm</option>
+									<option value="4:45 pm">4:45 pm</option>
+									<option value="5:00 pm">5:00 pm</option>
+									<option value="5:15 pm">5:15 pm</option>
+									<option value="5:30 pm">5:30 pm</option>
+									<option value="5:45 pm">5:45 pm</option>
+									<option value="6:00 pm">6:00 pm</option>
+									<option value="6:15 pm">6:15 pm</option>
+									<option value="6:30 pm">6:30 pm</option>
+									<option value="6:45 pm">6:45 pm</option>
+									<option value="7:00 pm">7:00 pm</option>
+									<option value="7:15 pm">7:15 pm</option>
+									<option value="7:30 pm">7:30 pm</option>
+									<option value="7:45 pm">7:45 pm</option>
+									<option value="8:00 pm">8:00 pm</option>
+									<option value="8:15 pm">8:15 pm</option>
+									<option value="8:30 pm">8:30 pm</option>
+									<option value="8:45 pm">8:45 pm</option>
+									<option value="9:00 pm">9:00 pm</option>
+									<option value="9:15 pm">9:15 pm</option>
+									<option value="9:30 pm">9:30 pm</option>
+									<option value="9:45 pm">9:45 pm</option>
+									<option value="10:00 pm">10:00 pm</option>
+									<option value="10:15 pm">10:15 pm</option>
+									<option value="10:30 pm">10:30 pm</option>
+									<option value="10:45 pm">10:45 pm</option>
+									<option value="11:00 pm">11:00 pm</option>
+									<option value="11:15 pm">11:15 pm</option>
+									<option value="11:30 pm">11:30 pm</option>
+									<option value="11:45 pm">11:45 pm</option>
+									<option value="12:00 am">12:00 am</option>
+									<option value="12:15 am">12:15 am</option>
+									<option value="12:30 am">12:30 am</option>
+								</select>
+								</li>
+								
+								<li>	
+								<label for="endStation">Station you end at:</label>
+								<select name="endStation" required>
+									<?php
+									listStations($xml_stationList);
+									?>
+								</select>
+								</li>
+							
+								<li>
+								<label>Phone number:</label>
+								<input type="text" name="phone" placeholder="415-NOT-LATE" /> <br />					</li>
+								
+								<li class="submit">
+								<input type="submit" class="btn primary" value="Sign Up">
+								</li>
+							
+							</ol>
+						</fieldset>
+					</form>
+				</div>
+			</div>
 	</section>
-	</div><!--/sectonBackground-->
 
 	<footer>
-		Late Bart Alert is in still in Beta testing. It is being built by Andrew Hyder, an <a href="http://hackyourcity.com">Urban Hacker</a> from San Francisco.
+		<div class="row">
+			<div class="span10 offset3">
+				Late Bart Alert is in still in Beta testing. It is being built by Andrew Hyder, an <a href="http://hackyourcity.com">Urban Hacker</a> from San Francisco.
+			</div>
+		</div>
 	</footer>
-	</div><!--/footerBackground-->
 
 </body>
 </html>
