56740c39dde0e845e36bc40a1f2f20619aa841d3
Andrew Hyder <hyd415@gmail.com>
2012-01-22 13:21:17 -0800

    Test alert.

--- testAlert.php
+++ testAlert.php
@@ -1,5 +1,5 @@
 <?php
 include sendAlerts.php;
 $currentAdvisory = 'Testing testing.';
-sendAlerts($currentAdvisory, list('4153074175')); // Send every alert to me.
+sendAlerts($currentAdvisory, array('4153074175')); // Send every alert to me.
 ?>
