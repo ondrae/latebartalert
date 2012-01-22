f27836282aaa43235d9a0de1c53899abcf83e867
Andrew Hyder <hyd415@gmail.com>
2012-01-22 13:54:04 -0800

    Added Goog Analytics.

--- index.php
+++ index.php
@@ -38,7 +38,20 @@ function echoArray($array){
 		<link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.3.0/bootstrap.min.css">
 		<LINK REL=StyleSheet HREF="lateBartStyle.css">
 		
-	
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-28537648-1']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		
+		</script>
+		
 	</head>
 	
 <body>
